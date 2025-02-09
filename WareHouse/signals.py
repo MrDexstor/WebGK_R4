346835dcf273e218141a703ea42a7c25f6a74fb9
NB@mrdexstor <nb-zenfen04@mail.ru>
2025-02-09 23:25:36 +0300

    Продакшен с анонсом

--- WareHouse/signals.py
+++ WareHouse/signals.py
@@ -3,6 +3,8 @@ from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from .models import Warehouse, Shelf, Product, Inventory, InventoryItem
 
+
+models = [Warehouse, Shelf, Product, Inventory, InventoryItem]
 # Словарь для хранения состояния записи до сохранения
 old_state = {}
 
@@ -31,70 +33,72 @@ def universal_pre_save(sender, instance, **kwargs):
 def universal_post_save(sender, instance, created, **kwargs):
     if getattr(instance, '_is_sync', False):
         return
-
-    change_type = 'create' if created else 'update'
-    log = {
-        "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        "changes": {
-            "app": sender._meta.app_label,
-            "table": sender.__name__,
-            "record_id": instance.id,
-            "change_type": change_type,
-            "old_state": old_state.pop(instance.pk, {}),
-            "new_state": {
-                field.name: getattr(instance, field.name)
-                if not field.is_relation else getattr(instance, field.name).pk
-                for field in instance._meta.fields
-                if not field.is_relation or field.many_to_one
+    if sender.__name__ in str(models.value()):
+        change_type = 'create' if created else 'update'
+        log = {
+            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "changes": {
+                "app": sender._meta.app_label,
+                "table": sender.__name__,
+                "record_id": instance.id,
+                "change_type": change_type,
+                "old_state": old_state.pop(instance.pk, {}),
+                "new_state": {
+                    field.name: getattr(instance, field.name)
+                    if not field.is_relation else getattr(instance, field.name).pk
+                    for field in instance._meta.fields
+                    if not field.is_relation or field.many_to_one
+                },
+                "updated_fields": []
             },
-            "updated_fields": []
-        },
-        "status": ""
-    }
+            "status": ""
+        }
 
-    # Обработка ManyToManyField
-    log["changes"]["new_state"].update({
-        field.name: list(getattr(instance, field.name).values_list('pk', flat=True))
-        for field in instance._meta.many_to_many
-    })
+        # Обработка ManyToManyField
+        log["changes"]["new_state"].update({
+            field.name: list(getattr(instance, field.name).values_list('pk', flat=True))
+            for field in instance._meta.many_to_many
+        })
 
-    # Определение измененных полей
-    old_data = log["changes"]["old_state"]
-    new_data = log["changes"]["new_state"]
-    log["changes"]["updated_fields"] = [
-        field for field in new_data
-        if new_data[field] != old_data.get(field)
-    ]
+        # Определение измененных полей
+        old_data = log["changes"]["old_state"]
+        new_data = log["changes"]["new_state"]
+        log["changes"]["updated_fields"] = [
+            field for field in new_data
+            if new_data[field] != old_data.get(field)
+        ]
 
-    # Здесь можно добавить логику для сохранения лога, например, в базу данных или файл
-    print(log)
+        # Здесь можно добавить логику для сохранения лога, например, в базу данных или файл
+        print(log)
 
 # Универсальный обработчик для post_delete
 @receiver(post_delete)
 def universal_post_delete(sender, instance, **kwargs):
-    try:
-        ids_rec = int(instance.id)
-    except Exception:
-        ids_rec = 0
-    log = {
-        "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        "changes": {
-            "app": sender._meta.app_label,
-            "table": sender.__name__,
-            "record_id": ids_rec,
-            "change_type": "delete",
-            "old_state": old_state.pop(instance.pk, {}),
-            "new_state": {},
-            "updated_fields": []
-        },
-        "status": ""
-    }
+    if sender.__name__ in str(models.value()):
 
-    # Здесь можно добавить логику для сохранения лога, например, в базу данных или файл
-    print(log)
+        try:
+            ids_rec = int(instance.id)
+        except Exception:
+            ids_rec = 0
+        log = {
+            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "changes": {
+                "app": sender._meta.app_label,
+                "table": sender.__name__,
+                "record_id": ids_rec,
+                "change_type": "delete",
+                "old_state": old_state.pop(instance.pk, {}),
+                "new_state": {},
+                "updated_fields": []
+            },
+            "status": ""
+        }
+
+        # Здесь можно добавить логику для сохранения лога, например, в базу данных или файл
+        print(log)
 
 # Подключение обработчиков к моделям
-models = [Warehouse, Shelf, Product, Inventory, InventoryItem]
+
 for model in models:
     pre_save.connect(universal_pre_save, sender=model)
     post_save.connect(universal_post_save, sender=model)
